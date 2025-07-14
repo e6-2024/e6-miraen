ab4ea7252b45e981c04f5677a011c5e971154c5a
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-07-14 19:52:50 +0900

    [update] 5-2-2 수정

--- client/src/pages/5-2-2.tsx
+++ client/src/pages/5-2-2.tsx
@@ -602,7 +602,7 @@ export default function Home() {
           enableZoom={!showIntro}
           enablePan={!showIntro}
           minDistance={0}
-          maxDistance={14}
+          maxDistance={6}
           maxPolarAngle={Math.PI/2}
           minPolarAngle={0}
         />
