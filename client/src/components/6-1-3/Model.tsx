99b810d9e9f0a4bc744580d63b2b91ac667980c7
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-09-22 14:12:34 +0900

    [update] 6-1-3

--- client/src/components/6-1-3/Model.tsx
+++ client/src/components/6-1-3/Model.tsx
@@ -10,13 +10,20 @@ type GLTFResult = GLTF & {
     GRASS_RoughGrass_Low_Mat_0: THREE.Mesh
     Plane002: THREE.Mesh
     Plane002_1: THREE.Mesh
-    SOIL_Mat_0001: THREE.Mesh
     TREE_A10_Mat_0003: THREE.Mesh
     TREE_A10_Mat_0003_1: THREE.Mesh
     TREE_A10_Mat_0003_2: THREE.Mesh
-    TREE_A10_Mat_0001_1: THREE.Mesh
-    TREE_A10_Mat_0001_2: THREE.Mesh
-    polySurface2001: THREE.Mesh
+    TREE_A10_Mat_0003_3: THREE.Mesh
+    TREE_A10_Mat_0003_4: THREE.Mesh
+    TREE_A10_Mat_0003_5: THREE.Mesh
+    TREE_A10_Mat_0003_6: THREE.Mesh
+    w: THREE.Mesh
+    Object_4: THREE.Mesh
+    SOIL_Mat_0001: THREE.Mesh
+    Mesh001: THREE.Mesh
+    Mesh001_1: THREE.Mesh
+    Mesh004: THREE.Mesh
+    Mesh004_1: THREE.Mesh
   }
   materials: {
     ['BroomSnakeweed_Cluster_Low_Mat.002']: THREE.Material
@@ -26,108 +33,106 @@ type GLTFResult = GLTF & {
     ['A10_Mat.002']: THREE.Material
     ['Material.007']: THREE.Material
     ['Material.008']: THREE.Material
+    ['lambert2SG.001']: THREE.Material
+    ['lambert2SG.002']: THREE.Material
+    ['lambert2SG.003']: THREE.Material
+    lambert2SG: THREE.Material
     material: THREE.Material
-    Material: THREE.Material
     ['Water_Pipe.001']: THREE.Material
+    ['Water_Pipe.002']: THREE.Material
+    ['Material.017']: THREE.Material
+    ['lambert2SG.016']: THREE.Material
   }
 }
 
 type ModelProps = JSX.IntrinsicElements['group'] & {
-  pipeScrollSpeed?: number   // 기본 0.25 (texels/sec 느낌)
-  pipeDirection?: 1 | -1     // 1 = +Y로 위로, -1 = 아래로
+  pipeScrollSpeed?: number
+  pipeDirection?: 1 | -1
 }
 
 export function Model({ pipeScrollSpeed = 0.8, pipeDirection = 1, ...props }: ModelProps) {
   const { nodes, materials } = useGLTF('/models/6-1-3/Tree.gltf') as GLTFResult
 
-  // 파이프 머티리얼을 clone해서 다른 메시에 영향 안 가게
-  const waterPipeMat = useMemo(() => {
-    const src = materials['Water_Pipe.001'] as THREE.MeshStandardMaterial | THREE.MeshPhysicalMaterial | THREE.MeshLambertMaterial | THREE.MeshPhongMaterial | THREE.MeshBasicMaterial
+  // 파이프 텍스처 스크롤용 머티리얼 복제
+  const waterPipeMat1 = useMemo(() => {
+    const src = materials['Water_Pipe.001'] as THREE.MeshStandardMaterial
     const cloned = src.clone()
-    // 텍스처 스크롤을 위해 랩 반복 설정
-    const map = (cloned as any).map as THREE.Texture | undefined
-    if (map) {
-      map.wrapS = THREE.RepeatWrapping
-      map.wrapT = THREE.RepeatWrapping
-      // 필요하면 반복 배수도 조절 가능 (패턴 크기)
-      // map.repeat.set(1, 1)
-      map.needsUpdate = true
+    if (cloned.map) {
+      cloned.map.wrapS = THREE.RepeatWrapping
+      cloned.map.wrapT = THREE.RepeatWrapping
+      cloned.map.needsUpdate = true
     }
     return cloned
   }, [materials])
 
-  // 파이프 메시에 접근해서 프레임마다 offset 갱신
-  const pipeRef = useRef<THREE.Mesh>(null!)
+  const waterPipeMat2 = useMemo(() => {
+    const src = materials['Water_Pipe.002'] as THREE.MeshStandardMaterial
+    const cloned = src.clone()
+    if (cloned.map) {
+      cloned.map.wrapS = THREE.RepeatWrapping
+      cloned.map.wrapT = THREE.RepeatWrapping
+      cloned.map.needsUpdate = true
+    }
+    return cloned
+  }, [materials])
+
+  const pipeRef1 = useRef<THREE.Mesh>(null!)
+  const pipeRef2 = useRef<THREE.Mesh>(null!)
 
   useFrame((_, delta) => {
-    const mat = waterPipeMat as THREE.MeshStandardMaterial
-    const map = mat.map
-    if (!map) return
-    // y 방향 스크롤 (direction으로 반전 가능)
-    map.offset.y = (map.offset.y + pipeDirection * pipeScrollSpeed * delta) % 1
-    // 일부 드라이버에서 즉시 반영 위해 필요할 때만
-    // map.needsUpdate = true
+    ;[waterPipeMat1, waterPipeMat2].forEach((mat) => {
+      const map = (mat as THREE.MeshStandardMaterial).map
+      if (map) {
+        map.offset.y = (map.offset.y + pipeDirection * pipeScrollSpeed * delta) % 1
+      }
+    })
   })
 
   return (
     <group {...props} dispose={null}>
       {/* 잔디 */}
       <group position={[-0.134, -1.156, 0.069]} scale={0.01}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.GRASS_BroomSnakeweed_Cluster_Low_Mat_0.geometry}
-          material={materials['BroomSnakeweed_Cluster_Low_Mat.002']}
-          position={[0, -8.281, 0]}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.GRASS_RoughGrass_Low_Mat_0.geometry}
-          material={materials['RoughGrass_Low_Mat.002']}
-          position={[-20.52, 0, 14.967]}
-        />
+        <mesh geometry={nodes.GRASS_BroomSnakeweed_Cluster_Low_Mat_0.geometry} material={materials['BroomSnakeweed_Cluster_Low_Mat.002']} position={[0, -8.281, 0]} />
+        <mesh geometry={nodes.GRASS_RoughGrass_Low_Mat_0.geometry} material={materials['RoughGrass_Low_Mat.002']} position={[-20.52, 0, 14.967]} />
       </group>
 
-      {/* 바닥/플레인 */}
+      {/* 바닥 */}
       <group position={[0.191, -0.636, -0.333]} rotation={[0, -1.529, 0]} scale={8.736}>
-        <mesh castShadow receiveShadow geometry={nodes.Plane002.geometry} material={materials['Material.001']} />
-        <mesh castShadow receiveShadow geometry={nodes.Plane002_1.geometry} material={materials['Material.006']} />
+        <mesh geometry={nodes.Plane002.geometry} material={materials['Material.001']} />
+        <mesh geometry={nodes.Plane002_1.geometry} material={materials['Material.006']} />
       </group>
 
       {/* 나무 */}
       <group position={[0.149, 2.98, -0.219]} scale={0.01}>
         <group position={[0, -34.143, 0]}>
-          <mesh castShadow receiveShadow geometry={nodes.TREE_A10_Mat_0003.geometry} material={materials['A10_Mat.002']} />
-          <mesh castShadow receiveShadow geometry={nodes.TREE_A10_Mat_0003_1.geometry} material={materials['Material.007']} />
-          <mesh castShadow receiveShadow geometry={nodes.TREE_A10_Mat_0003_2.geometry} material={materials['Material.008']} />
+          <mesh geometry={nodes.TREE_A10_Mat_0003.geometry} material={materials['A10_Mat.002']} />
+          <mesh geometry={nodes.TREE_A10_Mat_0003_1.geometry} material={materials['Material.007']} />
+          <mesh geometry={nodes.TREE_A10_Mat_0003_2.geometry} material={materials['Material.008']} />
+          <mesh geometry={nodes.TREE_A10_Mat_0003_3.geometry} material={materials['lambert2SG.001']} />
+          <mesh geometry={nodes.TREE_A10_Mat_0003_4.geometry} material={materials['lambert2SG.002']} />
+          <mesh geometry={nodes.TREE_A10_Mat_0003_5.geometry} material={materials['lambert2SG.003']} />
+          <mesh geometry={nodes.TREE_A10_Mat_0003_6.geometry} material={materials.lambert2SG} />
         </group>
-        <group position={[0, -34.143, 0]}>
-          <mesh castShadow receiveShadow geometry={nodes.TREE_A10_Mat_0001_1.geometry} material={materials['A10_Mat.002']} />
-          <mesh castShadow receiveShadow geometry={nodes.TREE_A10_Mat_0001_2.geometry} material={materials.Material} />
-        </group>
+        <mesh geometry={nodes.w.geometry} material={materials['A10_Mat.002']} position={[0, -34.143, 0]} />
       </group>
 
-      {/* 흙 */}
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.SOIL_Mat_0001.geometry}
-        material={materials.material}
-        position={[0, -0.494, 0]}
-        scale={0.01}
-      />
+      {/* 추가 오브젝트 */}
+      <mesh geometry={nodes.Object_4.geometry} material={materials.lambert2SG} position={[-1.613, 5.391, -2.918]} rotation={[-0.981, 0.797, 0.168]} scale={0.005} />
 
-      {/* 물 파이프 (텍스처 y-스크롤) */}
-      <mesh
-        ref={pipeRef}
-        castShadow
-        receiveShadow
-        geometry={nodes.polySurface2001.geometry}
-        material={waterPipeMat}
-        rotation={[Math.PI / 2, 0, 0]}
-        scale={0.01}
-      />
+      {/* 흙 */}
+      <mesh geometry={nodes.SOIL_Mat_0001.geometry} material={materials.material} position={[0, -0.494, 0]} scale={0.01} />
+
+      {/* 물 파이프 */}
+      <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+        <mesh ref={pipeRef1} geometry={nodes.Mesh001.geometry} material={waterPipeMat1} />
+        <mesh ref={pipeRef2} geometry={nodes.Mesh001_1.geometry} material={waterPipeMat2} />
+      </group>
+
+      {/* 다른 파이프 */}
+      <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+        <mesh geometry={nodes.Mesh004.geometry} material={materials['Material.017']} />
+        <mesh geometry={nodes.Mesh004_1.geometry} material={materials['lambert2SG.016']} />
+      </group>
     </group>
   )
 }
